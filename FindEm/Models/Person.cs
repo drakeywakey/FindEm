3422f6bfa8197f8b2b7c3e8dd23a0546a5e0beec
Drake Bennion <drakebennion@gmail.com>
2017-03-26 12:02:30 -0600

    Added more seed data, a little more styling

--- FindEm/Models/Person.cs
+++ FindEm/Models/Person.cs
@@ -61,7 +61,97 @@ namespace NoAuth.Models
                     ImagePath = "http://i.lv3.hbo.com/assets/images/series/westworld/character/dolores-1024.jpg"
                 });
 
-                foreach(Person person in persons)
+                persons.Add(new Person()
+                {
+                    FirstName = "Someone",
+                    LastName = "New",
+                    Age = 1,
+                    Address = "Earth",
+                    Interests = "Breathing, Eating, Acting",
+                    ImagePath = "/Content/Images/person1.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Drake",
+                    LastName = "Bennion",
+                    Age = 24,
+                    Address = "Murray",
+                    Interests = "Sleeping, Video Games, Crying",
+                    ImagePath = "https://lh6.googleusercontent.com/-aSR6vBntq54/AAAAAAAAAAI/AAAAAAAAAAA/bAAXNZ7ObxQ/s128-c-k/photo.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Dolores",
+                    LastName = "?",
+                    Age = -1,
+                    Address = "Westworld",
+                    Interests = "Breathing, Acting, Contemplating",
+                    ImagePath = "http://i.lv3.hbo.com/assets/images/series/westworld/character/dolores-1024.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Someone",
+                    LastName = "New",
+                    Age = 1,
+                    Address = "Earth",
+                    Interests = "Breathing, Eating, Acting",
+                    ImagePath = "/Content/Images/person1.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Drake",
+                    LastName = "Bennion",
+                    Age = 24,
+                    Address = "Murray",
+                    Interests = "Sleeping, Video Games, Crying",
+                    ImagePath = "https://lh6.googleusercontent.com/-aSR6vBntq54/AAAAAAAAAAI/AAAAAAAAAAA/bAAXNZ7ObxQ/s128-c-k/photo.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Dolores",
+                    LastName = "?",
+                    Age = -1,
+                    Address = "Westworld",
+                    Interests = "Breathing, Acting, Contemplating",
+                    ImagePath = "http://i.lv3.hbo.com/assets/images/series/westworld/character/dolores-1024.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Someone",
+                    LastName = "New",
+                    Age = 1,
+                    Address = "Earth",
+                    Interests = "Breathing, Eating, Acting",
+                    ImagePath = "/Content/Images/person1.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Drake",
+                    LastName = "Bennion",
+                    Age = 24,
+                    Address = "Murray",
+                    Interests = "Sleeping, Video Games, Crying",
+                    ImagePath = "https://lh6.googleusercontent.com/-aSR6vBntq54/AAAAAAAAAAI/AAAAAAAAAAA/bAAXNZ7ObxQ/s128-c-k/photo.jpg"
+                });
+
+                persons.Add(new Person()
+                {
+                    FirstName = "Dolores",
+                    LastName = "?",
+                    Age = -1,
+                    Address = "Westworld",
+                    Interests = "Breathing, Acting, Contemplating",
+                    ImagePath = "http://i.lv3.hbo.com/assets/images/series/westworld/character/dolores-1024.jpg"
+                });
+
+                foreach (Person person in persons)
                 {
                     context.Persons.Add(person);
                 }
