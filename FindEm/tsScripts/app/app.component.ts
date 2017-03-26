b4b0de71b4d006f74f8f780daad50398c1df338a
Drake Bennion <drakebennion@gmail.com>
2017-03-25 19:57:55 -0600

    Adding Angular2. Was this is a good idea? Time will tell.

---
+++ FindEm/tsScripts/app/app.component.ts
@@ -0,0 +1,32 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Person } from './person';
+import { AppService } from './app.service';
+
+@Component({
+    providers: [AppService],
+    selector: 'my-app',
+    template: `    
+    <input type="text" [(ngModel)]="search" />
+    <button (click)="getPeople()">Click me!</button>
+    <ul>
+      <li *ngFor="let person of people">
+        <img src="{{ person.ImagePath }}" />
+        <p>{{ person.FirstName }}</p>
+      </li>
+    </ul>
+  `
+})
+export class AppComponent implements OnInit {
+    constructor(private appService: AppService) { }
+    search = '';
+    people: Person[];
+
+    ngOnInit(): void {
+        this.getPeople();
+    }
+
+    getPeople(): void {
+        this.appService.getPeople(this.search).then(people => this.people = people);
+        console.log(this.people);
+    }
+}
---
+++ FindEm/tsScripts/app/app.service.ts
@@ -0,0 +1,23 @@
+﻿import { Injectable } from '@angular/core';
+import { Person } from './person';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class AppService {
+    private headers = new Headers({ 'Content-Type': 'application/json' });
+
+    constructor(private http: Http) { }
+
+    getPeople(search: string): Promise<Person[]> {
+        return this.http.post('/Person/Search', JSON.stringify({ searchString: search }), { headers: this.headers })
+            .toPromise()
+            .then(response => response.json() as Person[])
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+}
